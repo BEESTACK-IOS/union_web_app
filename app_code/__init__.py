5f859738c7667ddbfec598e6a903d8a0fa411878
arslanmustafabora <mboraarslan1999@gmail.com>
2021-01-02 18:11:48 +0300

    base of app

--- app_code/__init__.py
+++ app_code/__init__.py
@@ -9,6 +9,38 @@ con = psycopg2.connect(host="localhost", port="9999", database="tohumdb", user="
 app = Flask(__name__)
 app.secret_key = "boraadamdir"
 
+@app.route("/",methods=["GET","POST"])
+def index():
+    if "admin" in session:
+        return redirect(url_for("admin"))
+    elif "user" in session:
+        return redirect(url_for("user"))
+    else:
+        return redirect(url_for("login"))
+
+@app.route("/register",methods=["GET","POST"])
+def register():
+    pass
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    pass
+
+@app.route("/logout")
+def logout():
+    session.pop("admin",None)
+    session.pop("user",None)
+    session.pop("id",None)
+    return redirect(url_for("login"))
+
+@app.route("/admin",methods=["POST","GET"])
+def admin():
+    pass
+
+@app.route("/user", methods=["POST","GET"])
+def user():
+    pass
+
 
 
 
