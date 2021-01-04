63f983a4fe38615eccaf5267de2016a29a47ba0f
Ahmet Bahadir Eyuboglu <ahmetbahadireyuboglu@gmail.com>
2021-01-04 20:50:23 +0300

    updated and debug profil.js

--- static/js/profil.js
+++ static/js/profil.js
@@ -1,5 +1,5 @@
 $('#pass, #re_pass').on('keyup', function () {
-  if ($('#pass').val() == $('#re_pass').val()) {
+  if ($('#pass').val() == $('#re_pass').val() && $('#pass').val().length > 0) {
     $('#err_message').css('opacity', '0');
     $('#pass_message').html('Parolalar Uyumlu').css('color', 'green');
     $('#guncelle').removeAttr("type").attr("type", "submit");
@@ -10,11 +10,16 @@ $('#pass, #re_pass').on('keyup', function () {
     $('#guncelle').removeAttr("type").attr("type", "button");
     $('#guncelle').css('opacity', '0.5');
   }
+  if ($('#past_pass').val().length <= 0 ) {
+      $('#guncelle').removeAttr("type").attr("type", "button");
+      $('#guncelle').css('opacity', '0.5');
+      $('#past_pass_message').html('Parola Giriniz').css('color', 'red');
+  }
 });
 
 $('#past_pass').on('keyup', function () {
 
-    if ($('#past_pass').val().length <= 0) {
+    if ($('#past_pass').val().length <= 0 ) {
         $('#guncelle').removeAttr("type").attr("type", "button");
         $('#guncelle').css('opacity', '0.5');
         $('#past_pass_message').html('Parola Giriniz').css('color', 'red');
